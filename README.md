f323a46ff29a2e1702750015558bc65cbffb9b01
Adarsh Dixit <2021000450.aryan@ug.sharda.ac.in>
2024-06-06 20:12:01 +0530

    Adding Project folder and Requirements

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Automated-Attendance-system
+# End-to-end-Object-Detection-Project
+
+
+## How to run:
+
+```bash
+conda create -n autoatt python=3.7 -y
+```
+
+```bash
+conda activate autoatt
+```
+
+```bash
+pip install -r requirements.txt
+```
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'autoAttendance',
+    version= '0.0.0',
+    author= 'ARYAN-DIXIT1',
+    author_email= 'aryandixit1512@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
